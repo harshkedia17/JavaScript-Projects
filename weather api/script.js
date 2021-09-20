903c02c00eb9b0e49eb5902d5a39135ee27b144c
Harsh <harshkedia717@gmail.com>
2021-09-20 15:30:30 +0530

    Initial Commit

---
+++ weather api/script.js
@@ -0,0 +1,42 @@
+fetchWeather = (city) => {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=metric&appid=3ca00adc995880da38a771f3e4724473"
+  )
+    .then((response) => response.json())
+    .then((data) => displayWeather(data));
+};
+
+displayWeather = (data) =>{
+  const { name } = data
+  const { icon, description } = data.weather[0]
+  const { temp, humidity } = data.main
+  const { speed } = data.wind
+  console.log(name,icon,description,temp,humidity,speed)
+  document.querySelector(".city").innerHTML = "Weather in " + name 
+  document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png"
+  document.querySelector(".description").innerText = description;
+  document.querySelector(".temp").innerText = temp + "°C";
+  document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+  document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h"
+  
+  document.querySelector(".weather").classList.remove("loading");
+  document.body.style.backgroundImage =
+  "url('https://source.unsplash.com/1600x900/?" + name + "')";
+
+}
+search = ()=>{
+  this.fetchWeather(document.querySelector(".search-bar").value)
+}
+document.querySelector(".btn").addEventListener("click", function () {
+  search();
+});
+
+document.querySelector(".search-bar").addEventListener("keydown", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  })
+fetchWeather("Delhi")
